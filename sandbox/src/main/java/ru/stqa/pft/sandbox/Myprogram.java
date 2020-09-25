50f8a3225c44b92ee820cbdd2ef6dcf24de8f42c
Katerina-test <pticy83@mail.ru>
2020-09-25 21:42:20 +0300

    Update Myprogram.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Myprogram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Myprogram.java
@@ -1,9 +1,9 @@
 package ru.stqa.pft.sandbox;
 
-public class  Myprogram {
+public class Myprogram {
 
- public static void main (String[] args) {
-  System.out.println("Hello, world!");
- }
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 
 }
